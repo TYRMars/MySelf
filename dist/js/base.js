819c6b832895fd094d7a90e21d2a77f720df4a5e
张佳楠 <zhangjianan1996@qq.com>
2017-05-22 11:47:00 +0800

    边栏动画制作

--- dist/js/base.js
+++ dist/js/base.js
@@ -5,4 +5,20 @@
 {
     'use strict';
 
+    var sidebar = $('#sidebar'),
+        mask = $('.mask'),
+        sidebar_trigger =$('#sidebar_trigger');
+    
+    function showSidebar() {
+        mask.fadeIn();
+        sidebar.animate({'right':0});
+    }
+
+    function hideSidebar() {
+        mask.fadeOut();
+        sidebar.animate({'right': -300});
+    }
+
+    sidebar_trigger.on('click',showSidebar);
+    mask.on('click',hideSidebar);
 })
